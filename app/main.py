7f120ab1a5c1d561db860730976b0e7146a27674
Shinichi Okada <okada.shin@gmail.com>
2020-12-10 14:00:13 +0900

    Adding templates html and css

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
